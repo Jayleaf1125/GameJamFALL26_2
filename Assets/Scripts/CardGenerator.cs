266a60206db26ed9a9c6e39fd4046bba0ba840c6
Daniel Narvaez <github.uabx3@passfwd.com>
2026-09-14 17:15:46 -0400

    canvas organization

--- Assets/Scripts/CardGenerator.cs
+++ Assets/Scripts/CardGenerator.cs
@@ -9,7 +9,7 @@ public class CardGenerator : MonoBehaviour
     [SerializeField] List<VolunteerSO> _listOfVolunteers;
     [SerializeField] GameObject _canvas;
     [SerializeField] GameObject _cardPrefab;
-    [SerializeField] Transform _cardSpawn;
+    [SerializeField] RectTransform _playerHand;
 
     float _prevCardPosX;
 
@@ -18,24 +18,15 @@ public class CardGenerator : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
-        _prevCardPosX = _cardSpawn.position.x;
-
         foreach (VolunteerSO v in  _listOfVolunteers)
         {
-            GameObject card = Instantiate(_cardPrefab);
+            GameObject card = Instantiate(_cardPrefab, _playerHand);
             // Card Image
             card.transform.GetChild(1).GetComponent<Image>().sprite = v.Portrait;
             // Card Name
             card.transform.GetChild(2).GetComponent<TextMeshProUGUI>().text = v.Name;
-            // Child the card to Canvas
-            card.transform.SetParent(_canvas.transform, false);
             // Set Volunteer to card
             card.GetComponent<Card>().SetVolunteer(v);
-            // Move Card Over
-            RectTransform rect = card.GetComponent<RectTransform>();
-            rect.position = new Vector3(_prevCardPosX, rect.position.y, rect.position.z);
-            _prevCardPosX += 250f;
         }
     }
 
@@ -48,6 +39,6 @@ public class CardGenerator : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
-        Gizmos.DrawSphere(_cardSpawn.position, 0.5f);
+        Gizmos.DrawSphere(_playerHand.position, 0.5f);
     }
 }
